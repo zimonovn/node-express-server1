1cebf2dbe1ff1ef679c1734e4ff65a4190c993a1
Stigi888 <60792494+Stigi888@users.noreply.github.com>
2020-09-07 12:14:46 -0700

    Server

--- src/index.js
+++ src/index.js
@@ -1,20 +1,24 @@
-const express = require("express");
-
+import express from 'express';
 const app = express();
 const PORT = 5000;
 
-app.get("/", home);
-app.post("/info", info);
+app.get('/', home);
+app.post('/info', info);
+app.use(apiNotFound);
+
+function apiNotFound(req, res) {
+  res.status(400).send('API NOT found');
+}
 
 function home(req, res) {
-  res.send("Hello World!+++YaYayA");
+  res.send('Hello World!+++YaYayA');
 }
 
 function info(req, res) {
   const a = 123;
   const b = Math.random();
   const c = a + b;
-  res.send("INFO here NEW NEW", +c);
+  res.send('INFO here NEW NEW', +c);
 }
 
 app.listen(PORT, () => {
